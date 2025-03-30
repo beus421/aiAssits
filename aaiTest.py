1822c5852dab9743d9a2ae3dc7489c293a2d57ea
Hammad <163362079+beus421@users.noreply.github.com>
2025-03-30 23:39:49 +0500

    Update aaiTest.py

--- aaiTest.py
+++ aaiTest.py
@@ -1,12 +1,11 @@
 # `pip3 install assemblyai` (macOS)
 # `pip install assemblyai` (Windows)
-
+# testing assemblyai
 import assemblyai as aai
 
-aai.settings.api_key = "638750be2a2c4827965040c342c4fcce"
+aai.settings.api_key = "your api key"
 transcriber = aai.Transcriber()
 
-# transcript = transcriber.transcribe("https://storage.googleapis.com/aai-web-samples/news.mp4")
 transcript = transcriber.transcribe("succulentChineseMeal.wav")
 
 print(transcript.text)
